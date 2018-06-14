6b626993192f1c436964a74c8b300e4f4ed3b5e3
zhangwentao <zhangwentao@qtone.cn>
2018-06-14 11:25:26 +0800

    优化js逻辑，新增年月转换按钮，ec函数全面升级

--- js/run.js
+++ js/run.js
@@ -1,52 +1,12 @@
 define(function (require, exports) { //dedine闭包  
 	var loads=require("./load");//加载机制引用
-	//var loadData=require("./loadData");//图片资源引用
-    var funs=require("./qkyFunH5");
+	var jldata=require("./jlData");//数据导入
+    var funs=require("./jlfunction");//函数导入
 	
 	
 exports.jl = function () {
-	
-	funs.echartsGauge("jl-electric-ec1");
-	funs.echartsGauge("jl-electric-ec2");
-	funs.echartsGauge("jl-electric-ec3");
-	var weData={
-			line:{
-				tit:"消耗",
-				data:{"5.10":45,"5.11":55,"5.12":36,"5.13":28,"5.14":64,"5.15":51,"5.16":20,"5.17":71,"5.18":42,"5.19":41}
-			},
-			pie:{
-				tit:"耗能",
-				data:{"房1":[71,"#07c0c0 "],"房2":[29,"#ffb768"],"房3":[19,"#cf3b6f"]}
-			}
-	};
-	var eeData={
-		line:{
-			tit:"消耗",
-			data:{"5.10":45,"5.11":55,"5.12":36,"5.13":28,"5.14":64,"5.15":51,"5.16":20,"5.17":71,"5.18":42,"5.19":41}
-		},
-		pie:{
-			tit:"耗能",
-			data:{"房1":[71,"#07c0c0 "],"房2":[29,"#ffb768"],"房3":[19,"#cf3b6f"]}
-		}
-};
-var plData={
-	tit:["车位总数","剩余车位数","车流量","收费"],
-	data:{"6.11":55,"6.12":36,"6.13":28,"6.14":64,"6.15":51}
-};
-	funs.echartsPie("jl-wepie-ec",weData.pie.data,weData.pie.tit);
-	funs.echartsLine("jl-weline-ec",weData.line.data,weData.line.tit);
-	funs.echartsPie("jl-eepie-ec",eeData.pie.data,weData.pie.tit);
-	funs.echartsLine("jl-eeline-ec",eeData.line.data,weData.line.tit);
 
-	funs.echartsBar("jl-plline-ec",plData.data,plData.tit);
-	
-
-	var outindex=[
-		{id: "bg", src: "images/bg.png"},
-		{id: "logo", src: "images/logo.png"}
-		
-	];
-	loads.startPreload(outindex,function(){
+	loads.startPreload(jldata.loadimg,function(){
 		$(".loadprogress").fadeOut(200);
 		for (var key in loads.preload._loadItemsById) {
             $("." + key).html(loads.preload.getResult(key));
@@ -55,8 +15,45 @@ var plData={
 		window.onresize = function () {
 			loads.pageimg($(".page"), 1920, 1080);
 		}
-
+		loadafter();
 	})
+
+	function loadafter(){
+
+		funs.echartsLine("jl-eeline-ec",jldata.eeData.line);
+		funs.echartsPie("jl-eepie-ec",jldata.eeData.pie);
+		
+		funs.echartsLine("jl-weline-ec",jldata.weData.line);
+		funs.echartsPie("jl-wepie-ec",jldata.weData.pie);
+		
+
+		funs.echartsBar("jl-plline-ec",jldata.plData);
+		
+		funs.echartsGauge("jl-electric-ec1",13);
+		funs.echartsGauge("jl-electric-ec2",30);
+		funs.echartsGauge("jl-electric-ec3",40);
+
+		$(".jl-radio").on("click",".jl-rali",function(){
+			$(this).addClass("act").siblings().removeClass("act");
+			if($(this).parent().attr("tochange")=="ee"){
+				if($(this).attr("isdate")=="m"){
+					funs.echartsPie("jl-eepie-ec",jldata.eeData.pie);
+					funs.echartsLine("jl-eeline-ec",jldata.eeData.line);
+				}else{
+					funs.echartsPie("jl-eepie-ec",jldata.eeData.pieY);
+					funs.echartsLine("jl-eeline-ec",jldata.eeData.lineY);
+				}
+			}else{
+				if($(this).attr("isdate")=="m"){
+					funs.echartsPie("jl-wepie-ec",jldata.weData.pie);
+					funs.echartsLine("jl-weline-ec",jldata.weData.line);
+				}else{
+					funs.echartsPie("jl-wepie-ec",jldata.weData.pieY);
+					funs.echartsLine("jl-weline-ec",jldata.weData.lineY);
+				}
+			}
+		})
+	}
 		
 	
 		//var inbox = new Swiper(".inbox", {
@@ -75,10 +72,10 @@ var plData={
 		// 	}
 		// });
 
-		$(window).scroll(function(){
-			 var body_scr=document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+		// $(window).scroll(function(){
+		// 	 var body_scr=document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
 
-		});
+		// });
 
 	}
 
