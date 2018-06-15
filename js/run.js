6887caec8f78ed1cf23868a9804d3c1a752cc7f5
zawet <471032682@qq.com>
2018-06-15 18:34:05 +0800

    添加右侧滚动动画，并优化runjs和改善渲染机制

--- js/run.js
+++ js/run.js
@@ -1,8 +1,10 @@
 define(function (require, exports) { //dedine闭包  
 	var loads=require("./load");//加载机制引用
 	var jldata=require("./jlData");//数据导入
-    var funs=require("./jlfunction");//函数导入
+	var funs=require("./jlfunction");//函数导入
 	
+	var pdec=[];//缓存度数
+ 	var swpd,swsb,swgr;
 	
 exports.jl = function () {
 
@@ -16,23 +18,22 @@ exports.jl = function () {
 			loads.pageimg($(".page"), 1920, 1080);
 		}
 		loadafter();
+		
+		// setInterval(() => {
+		// 	loadafter();
+		// 	console.log("111");
+		// },2000);
+		
 	})
+}
 
 	function loadafter(){
 
+		//水能电能
 		funs.echartsLine("jl-eeline-ec",jldata.eeData.line);
 		funs.echartsPie("jl-eepie-ec",jldata.eeData.pie);
-		
 		funs.echartsLine("jl-weline-ec",jldata.weData.line);
 		funs.echartsPie("jl-wepie-ec",jldata.weData.pie);
-		
-
-		funs.echartsBar("jl-plline-ec",jldata.plData);
-		
-		funs.echartsGauge("jl-electric-ec1",13);
-		funs.echartsGauge("jl-electric-ec2",30);
-		funs.echartsGauge("jl-electric-ec3",40);
-
 		$(".jl-radio").on("click",".jl-rali",function(){
 			$(this).addClass("act").siblings().removeClass("act");
 			if($(this).parent().attr("tochange")=="ee"){
@@ -52,31 +53,58 @@ exports.jl = function () {
 					funs.echartsLine("jl-weline-ec",jldata.weData.lineY);
 				}
 			}
-		})
-	}
+		});
 		
+		//停车场
+		funs.echartsBar("jl-plline-ec",jldata.plData);
+		
+		//右边三排
+		mouldeDrwa("pdMoudle",jldata.pdData,"pd","swpd");
+		mouldeDrwa("sbMoudle",jldata.sbData,"sb","swsb");
+		mouldeDrwa("grMoudle",jldata.grData,"gr","swgr");
+		for(var ii=0;ii<pdec.length;ii++){funs.echartsGauge("jl-electric-ec"+ii,pdec[ii]);}
 	
-		//var inbox = new Swiper(".inbox", {
-			//initialSlide :1,
-			//pagination : '.swiper-pagination',
-			//paginationClickable :true,
-			//autoplay : 3000
-			
-		//});
+	}
 
-		// $(".icon-but a").click(function () {
-		// 	if ($(this).hasClass("left")) {
-		// 		inbox.slidePrev();
-		// 	} else {
-		// 		inbox.slideNext();
-		// 	}
-		// });
+	function mouldeDrwa(mID,data,drwaID,sw){
+		for(var i=0;i<data.length;i++){
+			$("#"+mID+" .roomNumber").html(data[i].roomNumber);
+			$("#"+mID+" .roomD").html(data[i].roomD);
+			$("#"+mID+" .roomRH").html(data[i].roomRH);
+			$("#"+mID+" .roomDr").html("");
+			for( var k=0;k<data[i].roomDr.length;k++){
+				$("#"+mID+".roomDr").append('<b >'+data[i].roomDr[k]+'  <span>A</span>');
+			}
 
-		// $(window).scroll(function(){
-		// 	 var body_scr=document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+			if(drwaID=="pd"){
+				$("#"+mID+" .jl-electric-ec").attr("id","jl-electric-ec"+i);
+				pdec.push(data[i].Ds);
 
-		// });
+				$("#"+mID+" .roomDl").html("");	
+				for( var j=0;j<data[i].roomDl.length;j++){
+					$("#"+mID+" .roomDl").append('<b >'+data[i].roomDl[j]+'  <span>°C</span>');
+				}
+			}else{
+				if(drwaID=="sb"){
+					$("#"+mID+" .roomHZ").html(data[i].roomHZ+"<span>HZ</span>");
+				}
+				if(data[i].runing){
+					$("#"+mID+" .runing").addClass("ing").removeClass("un").html("运行");
+					$("#"+mID+" .roomHZ,#"+mID+" .roomDr").removeClass("yc");
+					$("#"+mID+" .jcl-cont-icon").removeClass("un");
+				}else{
+					$("#"+mID+" .runing").addClass("un").removeClass("ing").html("停止");
+					$("#"+mID+" .roomHZ,#"+mID+" .roomDr").addClass("yc");
+					$("#"+mID+" .jcl-cont-icon").addClass("un");
+				}
+			}
+			$("."+drwaID+" .swiper-wrapper").append($("#"+mID).html());
+		}
 
+		sw = new Swiper("."+drwaID, {
+			slidesPerView :3,
+			autoplay : 3000
+		});
 	}
 
 });
